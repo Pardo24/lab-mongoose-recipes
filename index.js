cbe130f67e5810d417eff9c88466fb60e223cd3f
Dani Pardo <danielp.5@hotmail.com>
2022-06-23 16:50:23 +0200

    fet

--- index.js
+++ index.js
@@ -16,7 +16,34 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data)
+  })
+  .then((response) => {
+    console.log("agregando recetas")
+    response.forEach(e => {
+      console.log(e.title)
+    });
+   
+
+    return Recipe.create({title: "Ou dur", level: "Easy Peasy", ingredients: ["Ou", "Aigua"], cuisine:"Suso"
+  }) })
+  .then((response)=> {
+ console.log(response.title)
+ return Recipe.findOneAndUpdate(
+  {name: "Rigatoni alla Genovese"},
+  {duration: 100},
+  {new: true}
+  )
+  })
+
+  .then((response)=>{
+    console.log(response.duration);
+return Recipe.deleteOne({  name:  "Carrot Cake"  })
+  })
+
+  .then(()=>{
+    console.log("success");
+    return mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
